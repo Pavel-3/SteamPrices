df985b0da952bc2e98663a2041a5f97c6af55967
Pavel-3 <75582049+Pavel-3@users.noreply.github.com>
2021-10-11 23:14:07 +0300

    Add files via upload

---
+++ Colllections/Colllections/Item.cs
@@ -0,0 +1,70 @@
+﻿namespace Items
+{
+    enum Category
+    {
+        ConsumerGrade,
+        IndustrialGrade,
+        Blue,
+        Purple,
+        Pink,
+        Red
+    }    
+    public enum Exterior
+    {
+        FieldTested,
+        MinimalWear,
+        BattleScarred,
+        WellWorn,
+        FactoryNew
+    }
+    class Item
+    {
+        protected string id { get; set; }
+        protected Category _itemCategory;
+        private int _price;
+
+        public int Price
+        {
+            get { return _price; }
+            private set { _price = value; }
+        }
+
+        public Category ItemCategory
+        {
+            get { return _itemCategory; }
+            private set { _itemCategory = value; }
+        }
+
+        protected string nameEN;
+        protected string nameRU;
+
+    }
+
+    class Weapon : Item
+    {
+        Exterior _exterior;
+        public Weapon(string id, Category category, Exterior exterior)
+        {
+            this.id = id;
+            _category = category;
+            _exterior = exterior;
+        }
+    }
+
+    class Stiker : Item
+    {
+        public Stiker(string id, Category category)
+        {
+            this.id = id;
+            _category = category;
+        }
+    }
+    class Agent : Item
+    {
+        public Agent(string id, Category category)
+        {
+            this.id = id;
+            _category = category;
+        }
+    }
+}
---
+++ Colllections/Colllections/collections.cs
@@ -0,0 +1,137 @@
+﻿using Items;
+using System;
+using System.Collections.Generic;
+
+namespace collections
+{
+    class Collection
+    {
+        public Collection()
+        {
+            
+        }
+        const int RatioOfConsumerToRed = 3125;
+        const int RatioOfIndustrialToRed = 625;
+        const int RatioOfBlueToRed = 125;
+        const int RatioOfPurpleToRed = 25;
+        const int RatioOfPinkToRed = 5;
+        private double _probabilityOfConsumer;
+        private double _probabilityOfIndustrial;
+        private double _probabilityOfBlue;
+        private double _probabilityOfPurple;
+        private double _probabilityOfPink;
+        private double _probabilityOfRed;
+
+        public double CalculateTheExpectedValue(List<Item> collection)
+        {
+            CalculateProbability(collection);
+            double result;
+            result = 0;
+            return result;
+        }
+        private void CalculateProbability(List<Item> collection)
+        {
+            _probabilityOfConsumer = RatioOfConsumerToRed / SumOfOdds(collection);
+            _probabilityOfIndustrial = RatioOfIndustrialToRed / SumOfOdds(collection);
+            _probabilityOfBlue = RatioOfBlueToRed / SumOfOdds(collection);
+            _probabilityOfPurple = RatioOfPurpleToRed / SumOfOdds(collection);
+            _probabilityOfPink = RatioOfPinkToRed / SumOfOdds(collection);
+            _probabilityOfRed = 1 / SumOfOdds(collection);
+        }
+        private double SumOfOdds(List<Item> collection)
+        {
+            bool hasConsumerGrade = false;
+            bool hasIndustrialGrade = false;
+            bool hasBlue = false;
+            bool hasPurple = false;
+            bool hasPink = false;
+            bool hasRed = false;
+            foreach (Item item in collection)
+            { 
+                switch (item.Category)
+                {
+                    case Category.ConsumerGrade:
+                        hasConsumerGrade = true;
+                        break;
+                    case Category.IndustrialGrade:
+                        hasIndustrialGrade = true;
+                        break;
+                    case Category.Blue:
+                        hasBlue = true;
+                        break;
+                    case Category.Purple:
+                        hasPurple = true;
+                        break;
+                    case Category.Pink:
+                        hasPink = true;
+                        break;
+                    case Category.Red:
+                        hasRed = true;
+                        break;
+                }
+            }
+            return RatioOfConsumerToRed * Convert.ToInt32(hasConsumerGrade) + RatioOfIndustrialToRed * Convert.ToInt32(hasIndustrialGrade) +
+                RatioOfBlueToRed * Convert.ToInt32(hasBlue) + RatioOfPurpleToRed * Convert.ToInt32(hasPurple) +
+                RatioOfPinkToRed * Convert.ToInt32(hasPink) + Convert.ToInt32(hasRed);
+        }
+        private double CalculateTheArithmeticMeanOfConsumer(List<Item> collection)
+        {
+            (double, int) temp = FindSumOfTheCategory(collection, Category.ConsumerGrade);
+            return temp.Item1/temp.Item2;
+        }
+        private double CalculateTheArithmeticMeanOfIndustrial(List<Item> collection)
+        {
+            double result = 0;
+            //some code
+            return result;
+        }
+        private double CalculateTheArithmeticMeanOfBlue(List<Item> collection)
+        {
+            double result = 0;
+            //some code
+            return result;
+        }
+        private double CalculateTheArithmeticMeanOfPurple(List<Item> collection)
+        {
+            double result = 0;
+            //some code
+            return result;
+        }
+        private double CalculateTheArithmeticMeanOfPink(List<Item> collection)
+        {
+            double result = 0;
+            //some code
+            return result;
+        }
+        private double CalculateTheArithmeticMeanOfRed(List<Item> collection)
+        {
+            double result = 0;
+            //some code
+            return result;
+        }
+        private (double, int) FindSumOfTheCategory(List<Item> collection, Category category)
+        {
+            var temp = (sum:0.0, count:0);
+            foreach (Item item in collection)
+            {
+                if (item.ItemCategory == category)
+                {
+                    temp.sum += item.Price;
+                    temp.count++;
+                }
+            }
+            return temp;
+        }
+    }
+
+    class AgentCollection : Collection
+    {
+        AgentCollection()
+        {
+
+        }
+        List<Agent> _agentCollection;
+
+
+    }
+}
