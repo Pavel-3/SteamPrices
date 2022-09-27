2e7d4c7d60131d0eb1700197d8eeede3c1f2bf2e
Pavel Vouchyk <asdfgh32588@gmail.com>
2022-09-27 20:46:15 +0300

    some changes

--- Colllections/Collections/NonExteriorWeapon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Collections
-{
-    class NonExteriorWeapon : Item
-    {
-        public NonExteriorWeapon(int id, Category category, double price, string nameEN, string nameRU, string nameOfCollection)
-            : base(id, category, price, nameEN, nameRU, nameOfCollection) { }
-    }
-}
