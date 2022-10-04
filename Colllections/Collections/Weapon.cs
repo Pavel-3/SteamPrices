008c568b00945cad2d6c48b346d4ed6eb15d8846
Pavel Vouchyk <asdfgh32588@gmail.com>
2022-10-05 01:01:03 +0300

    class ExteriorWapon was removed and class Weapon was added

---
+++ Colllections/Collections/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Weapon : Item
+    {
+        public Weapon(int id, Category category, double price, string nameEN, string nameRU, string nameOfCollection)
+    : base(id, category, price, nameEN, nameRU, nameOfCollection) { }
+    }
+}
